21926cde610bc285a495cb2847838d7ef45d0333
moku95 <marcoromoli95@gmail.com>
2019-01-22 12:59:24 +0100

    Inizio lavoro gestione domande e risposte

---
+++ UntoldStories/Assets/Scripts/RoundData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class RoundData : MonoBehaviour
+{
+	public string name;
+	public int timeLimSec;
+	public QuestionData[] questions;
+}
